6bafe0ff02c294a02e4ad7d7276c895c7eb959b9
Amrothman5 <Amrothman5@gmail.com>
2021-11-24 23:00:31 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var audio = document.querySelector(".audio");
+audio.innerHTML=`<audio autoplay controls    class="audio2">
+<source src="http://41.33.11.25:8000/;"  type="audio/mpeg" >
+<source src="http://41.33.11.25:8000/;"  type="audio/ogg" >
+</audio>`;
+
