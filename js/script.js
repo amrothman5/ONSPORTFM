a5a19be0b2df373c289e1a40a633472765b7cdf8
Amrothman5 <Amrothman5@gmail.com>
2021-11-29 14:17:33 +0200

    first commit

--- js/script.js
+++ js/script.js
@@ -5,3 +5,8 @@ audio.innerHTML=`<audio autoplay controls    class="audio2">
 </audio>`;
 //https://carina.streamerr.co:2020/stream/OnSportFM
 //<source src="http://41.33.11.25:8000/;"  type="audio/mpeg" >
+var elms = document.getElementsByClassName( 'splide' );
+
+for ( var i = 0; i < elms.length; i++ ) {
+  new Splide( elms[ i ] ).mount();
+}
